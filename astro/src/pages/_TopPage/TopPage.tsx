1aeea807337b79618b694a6df5b27e68a62fd4d7
yosipy <todacchi.desu@gmail.com>
2024-07-14 22:57:03 +0900

    nits

--- astro/src/pages/_TopPage/TopPage.tsx
+++ astro/src/pages/_TopPage/TopPage.tsx
@@ -3,17 +3,16 @@ import type { FC } from "react"
 import "@mui/material-pigment-css/styles.css"
 
 // import Container from "@mui/material/Container"
-// import Container from "@mui/material-pigment-css/Container"
+import Container from "@mui/material-pigment-css/Container"
 // import Grid from "@mui/material-pigment-css/Grid"
 import Stack from "@mui/material-pigment-css/Stack"
 import CircularProgress from "@mui/material/CircularProgress"
 
 const A = (
   <>
-    {/* <Container maxWidth="md"> */}
-    <p>a</p>
-    <CircularProgress />
-    <Stack>
+    {/* <CircularProgress /> */}
+
+    {/* <Stack>
       <p>a</p>
       <p>a</p>
       <p>a</p>
@@ -26,13 +25,14 @@ const A = (
       <p>a</p>
       <p>a</p>
       <p>a</p>
-    </Stack>
-    {/* </Container> */}
+    </Stack> */}
+
+    <Container maxWidth="md">
+      <p>a</p>
+    </Container>
   </>
 )
 
 export const TopPage: FC = () => {
-  console.log("A")
-  // console.log(A)
   return A
 }
