5b9a5acdebaf7fb70620fdf950abb25d15d806e8
yosipy <todacchi.desu@gmail.com>
2024-07-14 22:39:22 +0900

    not working(v6)

---
+++ vite/src/TopPage/TopPage.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react"
+
+import Container from "@mui/material-pigment-css/Container"
+import Stack from "@mui/material-pigment-css/Stack"
+
+export const TopPage: FC = () => {
+  return (
+    <>
+      {/* <Stack>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+      </Stack> */}
+      <Container>aaaa</Container>
+    </>
+  )
+}
--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { pigment } from "@pigment-css/vite-plugin"
+import { extendTheme } from "@mui/material"
 
-const pigmentConfig = {}
+const pigmentConfig = { theme: extendTheme() }
 
 // https://vitejs.dev/config/
 export default defineConfig({
